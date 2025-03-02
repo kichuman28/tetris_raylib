c1aadea0015f05ce2f9ccc7e922eccefefa20110
Adwaith Jayasankar <kichu31084@gmail.com>
2025-03-02 20:56:39 +0530

    Update: Set up game loop and fps

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 
 int main() 
 {
-    InitWindow(300, 600, "Tetris");
-    SetTargetFPS(60);
+    InitWindow(300, 600, "Tetris");                     //Setting up the game window size
+    SetTargetFPS(60);                                   //Setting the fps of the game
 
-    
-    while(WindowShouldClose() == false){
 
-        BeginDrawing();
+    while(WindowShouldClose() == false){                //Starting of the game loop
+
+        BeginDrawing();                                 //All the elements are rendered in between these two lines of code
 
         EndDrawing();
 
