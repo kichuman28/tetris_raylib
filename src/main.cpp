ef4c16f33fdfa42153131ad9bd5f0f7f3f56179b
Adwaith Jayasankar <kichu31084@gmail.com>
2025-03-02 21:01:14 +0530

    Update: Added dark blue color for the background

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@ int main()
 {
     InitWindow(300, 600, "Tetris");                     //Setting up the game window size
     SetTargetFPS(60);                                   //Setting the fps of the game
-
+    Color darkBlue = {44,44,127,255};                   //Set dark blue color for the background
 
     while(WindowShouldClose() == false){                //Starting of the game loop
 
         BeginDrawing();                                 //All the elements are rendered in between these two lines of code
-
+        ClearBackground(darkBlue);                      //Added the dark blue color to the background
         EndDrawing();
     }
 
